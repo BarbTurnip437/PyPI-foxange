9040e08eaaea477899d64a9acc2590586c0a8466
BarbTurnip437 <zta-4ucsk8dla@dingtalk.com>
2026-06-03 20:43:55 +0800

    删除一个不知意味的文件

--- src/foxange/__init__.py
+++ src/foxange/__init__.py
@@ -1,13 +1,5 @@
 """Github repo: https://github.com/foxange-org/PyPI-foxange"""
 
-from warnings import deprecated
-
-from . import file, input, list_proce, math
-from .file import input_to_file
-from .input import collect_input
-from .list_proce import unique
-from .math import sum
-
 __all__ = [
     'file',
     'input',
@@ -19,8 +11,15 @@ __all__ = [
     'sum',
 ]
 
-
 __version__ = '0.5.0'
+
+from warnings import deprecated
+
+from . import file, input, list_proce, math
+from .file import input_to_file
+from .input import collect_input
+from .list_proce import unique
+from .math import sum
 
 
 # 为向下兼容而保留，不过说实话删了更好
